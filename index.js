875a9b17932520c7f61505a9ec5d7985f31d7e78
AJ <aj@smartnvm.com>
2021-09-26 20:44:38 +0000

    tinker with light coin

--- index.js
+++ index.js
@@ -1,29 +1,100 @@
-let balance = 500.00;
+/*
+Here's a list of features that our code needs to support:
 
-class Withdrawal {
+Allow multiple accounts to be created  -> class account
+Each account can have many transactions  -> property
+Allow withdrawals and deposits into accounts  -> methods
+Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+Allow us to retrieve the current balance of the account at any time
+Don't allow withdrawals that exceed the remaining balance of the account
 
-  constructor(amount) {
-    this.amount = amount;
+
+*/
+
+
+class Account {
+  constructor() {
+    //this.account = username;
+    this.transactions = []
+    this.balance = 0;
   }
 
-  commit() {
-    balance -= this.amount;
+  getbalance() {
+    this.balance = 0
+    for (let t of this.transactions) {
+      this.balance += t.val
+    }
+    return this.balance;
+  }
+
+
+  addTranaction(transaction) {
+    this.transactions.push(transaction)
+  }
+
+  history() {
+    console.log("Transaction History\n", this.transactions)
   }
 
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
 
+  }
+
+  commit() {
+    if (!this.checkNSF()) return false;
+
+    this.time = new Date();
+    this.account.addTranaction(this)
+  }
+}
+
+class Deposit extends Transaction {
+
+  get val() {
+    //this.account.balance +=this.amount;
+    return this.amount;
+  }
+
+  checkNSF() {
+    // note how it has access to this.account b/c of parent
+    return true
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get val() {
+    //console.log('with',this.val)
+    return -this.amount;
+
+  }
+
+  checkNSF() {
+    // note how it has access to this.account b/c of parent
+    let runningBalance = this.account.getbalance()
+    //console.log('running:',runningBalance - this.amount >=0 )
+    return  (runningBalance - this.amount >= 0);
+  }
+}
 
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('Sponge Bob');
+
+console.log('Starting Balance:', myAccount.getbalance());
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('ending balance:', myAccount.getbalance())
+//console.log(myAccount.transactions);
